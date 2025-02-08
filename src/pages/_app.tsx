b679d7c485ed88216ef005a39a27b80d37aa5584
M0hammedK <mmm2016alkaf@gmail.com>
2025-02-08 15:14:32 +0300

    .

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import Layout from "@/components/common/Layout";
 import "@/styles/globals.css";
+import { deleteCookie } from "cookies-next";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
+  deleteCookie("uaerRole");
+  deleteCookie("accessToken");
   return (
     <Layout>
       <Component {...pageProps} />;
